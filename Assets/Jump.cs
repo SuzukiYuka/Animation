18b0c4edf80fd7b91b8efe43cff4e168c87d9225
Yuka Suzuki <suzukiyuka1218@gmail.com>
2017-09-20 11:56:15 +0900

    first commit

---
+++ Assets/Jump.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jump : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        var animator = GetComponent<Animator>();
+        if (Input.GetKey(KeyCode.Space)) {
+            animator.SetBool("Jump", true);
+        } else {
+            animator.SetBool("Jump", false);
+        }
+	}
+}
